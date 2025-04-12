d1735370a475b41ea10e6d02535c24fcdbd90c15
Lydai <65956871+ly27253@users.noreply.github.com>
2025-04-12 11:51:15 +0800

    Update model_utils.py

--- mmseg/models/backbones/model_utils.py
+++ mmseg/models/backbones/model_utils.py
@@ -1,4 +1,8 @@
-> ⚠️ **Code Notice**  
-> This repository will include the core implementation after the associated paper is accepted.  
-> Thank you for your patience!
-> As of 2025-04-11 Manuscript under peer review
+🚧 Code Availability Notice
+🛠️ Note:
+This script includes core components of our implementation.
+The code has been fully prepared for release, but is currently withheld due to intellectual property considerations during the review process.
+We sincerely apologize for the inconvenience.
+🔓 The full code will be made publicly available immediately upon acceptance of the related paper.
+
+Thank you for your understanding and support.
